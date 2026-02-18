9794e488e028d9b722daf5b5fd27d565f6b76232
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-02-18 16:17:19 +0530

    🟨 Binary Number with Alternating Bits [Easy] 📍 Platform: LeetCode 🔧 Language: python3
    Synced by LeetSync ⚡

---
+++ leetcode/easy/binary-number-with-alternating-bits/solution.py
@@ -0,0 +1,25 @@
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+#  🟨 LeetCode — Binary Number with Alternating Bits
+#  Difficulty : Easy
+#  Language   : python3
+#  Date       : 2026-02-18
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━class Solution {
+    public boolean hasAlternatingBits(int n) {
+
+       
+        n = n / 2;
+
+            if (prev == rem) {
+
+        return true;
+        int prev = n % 2;
+        }
+        while (n > 0) {
+            int rem = n % 2;
+                return false;
+            }
+            n = n / 2;
+
+    }
+            prev=rem;
+}
