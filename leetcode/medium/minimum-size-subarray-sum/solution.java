f8519f66161da6331f7ca315d35840815a8b35fa
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-02-21 18:22:16 +0530

    🟨 minimum-size-subarray-sum [Medium] 📍 Platform: LeetCode 🔧 Language: java
    Synced by LeetSync ⚡

--- leetcode/medium/minimum-size-subarray-sum/solution.java
+++ leetcode/medium/minimum-size-subarray-sum/solution.java
@@ -3,20 +3,24 @@
 //  Difficulty : Medium
 //  Language   : java
 //  Date       : 2026-02-21
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+
+     
+        int n = nums.length;
+
+        int min =Integer.MAX_VALUE , sum=0;
+       
+        int left = 0;
+
+        // for(int i=0; i<n ;i++){
+           
+        // }
+
        for(int right=0; right<n ;right++){
         sum += nums[right];
 
         while(sum >= target){
+            min = Math.min(min ,right-left+1);
         
             sum -= nums[left];
-            min = Math.min(min ,right-left+1);
-            left++;
-          }
-
-       }
-
-       return min == Integer.MAX_VALUE ? 0 : min;
-        
-    }
-}
