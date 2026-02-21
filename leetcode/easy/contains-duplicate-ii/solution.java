87183c08cd5e852329c0ab6e40f08ab2789fee9f
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-02-21 12:01:21 +0530

    🟨 contains-duplicate-ii [Easy] 📍 Platform: LeetCode 🔧 Language: java
    Synced by LeetSync ⚡

---
+++ leetcode/easy/contains-duplicate-ii/solution.java
@@ -0,0 +1,25 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//  🟨 LeetCode — contains-duplicate-ii
+//  Difficulty : Easy
+//  Language   : java
+//  Date       : 2026-02-21
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━        return false;
+            map.put(nums[i], i);
+
+
+        }
+            }
+
+                    return true;
+                }
+
+    }
+                if (abs <= k) {
+                int abs = Math.abs(i - map.get(nums[i]));
+            if (map.containsKey(nums[i])) {
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+}
