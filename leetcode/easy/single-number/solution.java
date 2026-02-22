3cb72e5505f51421678e5b5bf623e80fd82df45b
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-02-22 21:12:02 +0530

    🟨 single-number [Easy] 📍 Platform: LeetCode 🔧 Language: java
    Synced by LeetSync ⚡

---
+++ leetcode/easy/single-number/solution.java
@@ -0,0 +1,27 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//  🟨 LeetCode — single-number
+//  Difficulty : Easy
+//  Language   : java
+//  Date       : 2026-02-22
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        // for (int num : nums) {
+
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+
+        // }
+
+        // for (Map.Entry<Integer, Integer> x : map.entrySet()) {
+        //     if (x.getValue() == 1) {
+        //         return x.getKey();
+        //     }
+        // }
+         
+
+    }
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        int xor =0;
+        for (int num : nums) {
+        }
+            xor ^=num;
+        return xor;
