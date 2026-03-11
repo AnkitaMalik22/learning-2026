4675dad417cb65317eb0127d712054ba3de9f553
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-03-11 23:44:04 +0530

    🟨 intersection-of-two-linked-lists [Easy] 📍 Platform: LeetCode 🔧 Language: java
    Synced by LeetSync ⚡

---
+++ leetcode/easy/intersection-of-two-linked-lists/solution.java
@@ -0,0 +1,21 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//  🟨 LeetCode — intersection-of-two-linked-lists
+//  Difficulty : Easy
+//  Language   : java
+//  Date       : 2026-03-11
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━                a = a.next;
+            }
+            if (b == null) {
+            } else {
+                b = b.next;
+            }
+                b = headA;
+            } else {
+
+                a = headB;
+            if (a == null) {
+
+        }
+        return a;
+    }
+}
