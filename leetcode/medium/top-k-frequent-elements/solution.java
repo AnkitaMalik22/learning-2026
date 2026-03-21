3e4028d603effd9e0ce17e391307f1e9d5b0238f
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-03-21 23:19:00 +0530

    🟨 top-k-frequent-elements [Medium] 📍 Platform: LeetCode 🔧 Language: java
    Synced by LeetSync ⚡

---
+++ leetcode/medium/top-k-frequent-elements/solution.java
@@ -0,0 +1,26 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//  🟨 LeetCode — top-k-frequent-elements
+//  Difficulty : Medium
+//  Language   : java
+//  Date       : 2026-03-21
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━                pq.poll();
+
+            }
+        }
+
+        int[] res = new int[k];
+
+        // for( int i=0 ; i<k ; i++){
+        //     res[i] = pq.poll();
+
+        // }
+        int i = 0;
+            if (pq.size() > k) {
+
+            pq.offer(num);
+        for (int num : map.keySet()) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+
+        // 3- 1
+        // 2-2
