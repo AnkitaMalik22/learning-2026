e9ebb9f9f7389f054958a6362c760e5225c12aa7
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2026-04-09 23:33:12 +0530

    🟨 find-the-duplicate-number [Medium] 📍 Platform: LeetCode 🔧 Language: java
    Synced by LeetSync ⚡

---
+++ leetcode/medium/find-the-duplicate-number/solution.java
@@ -0,0 +1,26 @@
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//  🟨 LeetCode — find-the-duplicate-number
+//  Difficulty : Medium
+//  Language   : java
+//  Date       : 2026-04-09
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+
+            } else {
+                i++;
+                int temp = nums[i];
+
+                }
+                    return nums[i];
+
+                if (nums[i] == nums[correctIndex]) {
+                int correctIndex = nums[i] - 1;
+
+            if (nums[i] != i + 1) {
+        while (i < n) {
+
+        int i = 0;
+        int n = nums.length;
+            }
+
+        }
